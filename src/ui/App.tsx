f334a44d9cee6c07424f443eb7b00de075553deb
Friedchicken <paololego20+github@gmail.com>
2022-01-28 16:54:37 +0100

    fix userInputs

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React  from 'react'
 import './App.css';
 import { Game } from './Game'
 import { UserInput } from './UserInput'
 
+function Main() {
+    return <div className="Main">
+        <UserInput />
+        <Game />
+        <div />
+    </div>
+}
+
 function App() {
     return (
         <div className="App">
-            <UserInput />
-            <Game />
-            <div />
+            <Main />
         </div>
     );
 }
--- src/ui/UserInput.tsx
+++ src/ui/UserInput.tsx
@@ -5,46 +5,70 @@ import store from '../store'
 type InputProps = {
     name: string
     value: number 
+    min: number
+    max: number | undefined
 }
 
-function Input({name, value }: InputProps) {
-    const [v, setValue] = useState(value)
+function Input({ name, value, min, max }: InputProps) {
+    const [v, setValue] = useState<string>(value.toString())
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const num = parseFloat(e.target.value)
-        setValue(num)
+        setValue(e.target.value)
     }
 
-    const save = (a: string, b: number) => store.dispatch({type: 'input', payload: {[a]: b}})
+    const save = (a: string, b: string) => {
+        const num = parseFloat(b)
+        if(num >= min && (max === undefined || num <= max))
+            store.dispatch({type: 'input', payload: {[a]: num}})
+        else 
+            setValue(store.getState().input[a])
+    }
 
     return <div className="input-element">
         <span>{name.charAt(0).toUpperCase() + name.slice(1)}</span>
         <input
             type="number"
+            min={min}
             id={name}
             value={v || ''}
             onChange={handleChange}
-            onBlur={() => save(name, v || 0)}
-            onKeyUp={(e) => e.key === 'Enter' && save(name, v || 0)}
+            onBlur={() => save(name, v || '')}
+            onKeyUp={(e) => e.key === 'Enter' && save(name, v || '')}
         />
     </div>
 }
 
 export function UserInput() {
-    const base = {
-        "movement": .5,
-        "rotation": 45,
-        "threshold": 10,
-    }
+    const base = [
+        {
+            name: "movement",
+            value: .5,
+            min: .01,
+            max: 1,
+        },
+        {
+            name: "rotation",
+            value: 45,
+            min: 1,
+        },
+        {
+            name: "threshold",
+            value: 10,
+            min: 1,
+            max: 10,
+        }
+    ]
 
-    Object.entries(base).map(([key, value]) => store.dispatch({type: 'input', payload: {[key]: value}}))
+    base.forEach(({ name, value }) => store.dispatch({type: 'input', payload: {[name]: value}}))
     
     return <div className="container-input" >
-        {Object.entries(store.getState().input).map(
-            ([key, value]) => <Input
-                key={key}
-                name={key}
+        {base.map(
+            ({ name, value, min, max }) => <Input
+                key={name}
+                name={name}
                 value={value as number}
+                min={min}
+                max={max}
             />
         )}
     </div>
